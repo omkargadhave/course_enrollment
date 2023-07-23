3db95cbc88098121ec4912548c0d4771df22398d
omkargadhave <87654298+omkargadhave@users.noreply.github.com>
2023-07-23 14:41:42 +0530

    Add files via upload

---
+++ Models/Course.cs
@@ -0,0 +1,9 @@
+public class Course
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+
+    
+    public ICollection<StudentCourse> StudentCourses { get; set; }
+}
